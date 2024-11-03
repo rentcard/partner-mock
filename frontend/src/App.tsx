c0a08b122c264502e1323e3ff91b906cb010d787
Martin Diessner <martin.diessner@rentcard.id>
2024-11-03 15:49:25 +0100

    updated FE for redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
   const feUrl = process.env.REACT_APP_FE_URL;
-  const [userData, setUserData] = useState('');
+  const [userData, setUserData] = useState("");
   const [loading, setLoading] = useState(false);
   const applicantId = "1234567";
 
@@ -12,28 +12,28 @@ function App() {
     setLoading(true);
     try {
       const response = await fetch(`${apiBaseUrl}/app/auth/rentcard/webhook`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        applicantId: applicantId,
-        operation: "UPDATE",
-      }),
-    });
-    const data = await response.json();
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          applicantId: applicantId,
+          operation: "UPDATE",
+        }),
+      });
+      const data = await response.json();
       setUserData(data);
     } catch (error) {
-      console.error('There was an error fetching the data:', error);
-      setUserData('Failed to load data.'); 
+      console.error("There was an error fetching the data:", error);
+      setUserData("Failed to load data.");
     } finally {
       setLoading(false);
     }
   };
-  
+
   const iframeSrc = `${feUrl}/jump?user=${encodeURIComponent(
     JSON.stringify({
-      objectId: "2250346",
+      objectId: "22504346",
       applicantId: applicantId,
       rent: "1000",
       deposit: "3000",
@@ -44,20 +44,27 @@ function App() {
 
   return (
     <div className="App">
-      <div><h1>This is the partner website</h1>
-      <iframe
-        className="bg-transparent"
-        title="frame"
-        id="ifrm"
-        width="100%"
-        height="400px"
-        src={iframeSrc}
-        style={{ overflowY: "hidden", border: "none" }}
-      ></iframe></div>
       <div>
-        <h1>Get User data</h1>
-        <div>Click the link to see the simulated webhook response after completing the OAuth2 flow</div>
-        <a href="#!" onClick={fetchData}>{loading ? 'Loading...' : 'Get data'}</a>
+        <h1>This is what the user sees</h1>
+        <iframe
+          className="bg-transparent"
+          title="frame"
+          id="ifrm"
+          width="100%"
+          height="800px"
+          src={iframeSrc}
+          style={{ overflowY: "hidden", border: "none" }}
+        ></iframe>
+      </div>
+      <div>
+        <h1>Here is the partner view</h1>
+        <div>
+          Click the link to see the simulated webhook response after completing
+          the OAuth2 flow
+        </div>
+        <a href="#!" onClick={fetchData}>
+          {loading ? "Loading..." : "Get data"}
+        </a>
         {userData && Object.keys(userData).length > 0 && (
           <div>
             <h2>User Data:</h2>
@@ -65,7 +72,6 @@ function App() {
           </div>
         )}
       </div>
-
     </div>
   );
 }
